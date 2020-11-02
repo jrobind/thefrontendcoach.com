33a26a94b13ee268d6fde64564ef997c1b6151ae
James Robinson <jrobind@outlook.com>
2020-11-02 12:16:45 +0000

    fix newsletter & footer layouts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ export default function Footer() {
       <svg className="wave" viewBox="-606.652 143.223 1905.641 147.109" xmlns="http://www.w3.org/2000/svg">
         <path  d="M 826.337 25.54 C 678.546 84.297 603.696 68.787 447.802 35.144 C 293.343 1.811 137.334 1.811 0 1.811 L 0 150 L 1920 150 L 1920 1.811 C 1739.535 -16.685 1566.81 182.718 1378.127 82.941 C 1189.444 -16.836 974.129 -33.218 826.337 25.54 Z" fill="#2B8275" transform="matrix(1, 0, 0, 1, -615.011647, 153.801998)"/>
       </svg>
-      <footer className="footer text-xs md:text-sm bg-main text-white pb-6 py-3 px-3">
-        <div className="wrapper flex pb-3">
+      <footer className="footer text-sm bg-main text-white pb-6 py-3 px-3">
+        <div className="wrapper flex pb-3 flex-wrap">
           <div className="mr-3">
             <ul>
               <li className="mr-2 mb-2 mt-2">
@@ -26,12 +26,12 @@ export default function Footer() {
           </div>
           <div className="mr-3">
             <ul>
-              <li className="m-2">
+              <li className="mr-2 mb-2 mt-2">
                 <Link href="/blog">
                   <a>Blog</a>
                 </Link>
               </li>
-              <li className="m-2">
+              <li className="mr-2 mb-2 mt-2">
                 <Link href="/podcast">
                   <a>Podcast</a>
                 </Link>
@@ -40,8 +40,8 @@ export default function Footer() {
           </div>
           <div>
             <ul>
-              <li className="m-2">james@thefrontendcoach.com</li>
-              <li className="m-2">
+              <li className="mr-2 mb-2 mt-2">james@thefrontendcoach.com</li>
+              <li className="mr-2 mb-2 mt-2">
                 <Link href="https://www.iubenda.com/privacy-policy/79995354">
                   <a className="iubenda-white iubenda-embed" title="Privacy Policy">Privacy Policy</a>
                 </Link>
