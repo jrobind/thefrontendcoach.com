183624108b6db594870c3a565c903a7684921890
Kate Johnson <jrobind@outlook.com>
2020-11-09 19:04:40 +0000

    add twitter icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,52 +9,59 @@ export default function Footer() {
         <path  d="M 826.337 25.54 C 678.546 84.297 603.696 68.787 447.802 35.144 C 293.343 1.811 137.334 1.811 0 1.811 L 0 150 L 1920 150 L 1920 1.811 C 1739.535 -16.685 1566.81 182.718 1378.127 82.941 C 1189.444 -16.836 974.129 -33.218 826.337 25.54 Z" fill="#2B8275" transform="matrix(1, 0, 0, 1, -615.011647, 153.801998)"/>
       </svg>
       <footer className="footer text-sm bg-main text-white pb-6 py-3 px-3">
-        <div className="wrapper flex pb-3 flex-wrap">
-          <div className="mr-3">
-            <ul>
-              <li className="mr-2 mb-2 mt-2">
-                <Link href="/about">
-                  <a className="p-1">About</a>
-                </Link>
-              </li>
-              <li className="mr-2 mb-2 mt-2">
-                <Link href="/support">
-                  <a className="p-1">Support</a>
-                </Link>
-              </li>
-            </ul>
+        <div className="wrapper">
+          <div className="flex flex-wrap">
+            <div className="mr-3">
+              <ul>
+                <li className="mr-2 mb-2 mt-2">
+                  <Link href="/about">
+                    <a className="p-1">About</a>
+                  </Link>
+                </li>
+                <li className="mr-2 mb-2 mt-2">
+                  <Link href="/support">
+                    <a className="p-1">Support</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="mr-3">
+              <ul>
+                <li className="mr-2 mb-2 mt-2">
+                  <Link href="/blog">
+                    <a className="p-1">Blog</a>
+                  </Link>
+                </li>
+                <li className="mr-2 mb-2 mt-2">
+                  <Link href="/podcast">
+                    <a className="p-1">Podcast</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li className="mr-2 p-1 mt-1">james@thefrontendcoach.com</li>
+                <li className="mr-2 mb-2 p-1">
+                  <Link href="https://www.iubenda.com/privacy-policy/79995354">
+                    <a className="iubenda-white iubenda-embed" title="Privacy Policy">Privacy Policy</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="mr-3">
-            <ul>
-              <li className="mr-2 mb-2 mt-2">
-                <Link href="/blog">
-                  <a className="p-1">Blog</a>
-                </Link>
-              </li>
-              <li className="mr-2 mb-2 mt-2">
-                <Link href="/podcast">
-                  <a className="p-1">Podcast</a>
-                </Link>
-              </li>
-            </ul>
+          <div className="flex items-center pt-2 pb-4">
+            <a href="https://github.com/jrobind" className="mr-3 pl-1">
+              <img alt="GitHub icon" src="/images/github-icon.png" width="18" height="18" style={{ marginTop: '-4px'}}/>
+            </a>
+            <a href="https://twitter.com/FrontEndCoach">
+              <img alt="Twitter icon" src="/images/twitter-icon.svg" width="18" height="18" style={{ marginTop: '-4px'}}/>
+            </a>
           </div>
-          <div>
-            <ul>
-              <li className="mr-2 p-1 mt-1">james@thefrontendcoach.com</li>
-              <li className="mr-2 mb-2 p-1">
-                <Link href="https://www.iubenda.com/privacy-policy/79995354">
-                  <a className="iubenda-white iubenda-embed" title="Privacy Policy">Privacy Policy</a>
-                </Link>
-              </li>
-            </ul>
+          <div className="pt-3 wrapper flex flex-wrap" style={{borderTop: '1px solid rgba(250, 250, 250, 0.45)'}}>
+            <span className="mr-4 mt-2">The Front End Coach 2020</span>
+            <span className="mt-2">Built by James Robinson</span>
           </div>
-        </div>
-        <div className="pt-3 wrapper flex flex-wrap" style={{borderTop: '1px solid rgba(250, 250, 250, 0.45)'}}>
-          <span className="mr-4 mt-2">The Front End Coach 2020</span>
-          <span className="mt-2 flex">
-            Built by James Robinson
-            <a className="flex items-center" href="https://github.com/jrobind"><img className="ml-2" alt="GitHub icon" src="/images/github-icon.png" width="18" height="18" style={{ marginTop: '-4px'}}/></a>
-          </span>
         </div>
       </footer>  
     </>
