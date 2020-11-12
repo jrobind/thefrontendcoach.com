b3f3776f46cd9e5acc4a9e22655c7a2490107b4f
Kate Johnson <jrobind@outlook.com>
2020-11-12 15:47:04 +0000

    remove packages and unused fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,12 @@
-const browserSync = require('browser-sync');
-const server = browserSync.create();
-
-const gulp = require('gulp');
-const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
-const concat = require('gulp-concat');
-const sass = require('gulp-sass');
 const { src, series, parallel, dest, watch } = require('gulp');
-
-sass.compiler = require('node-sass');
 
 const IMG_PATH = 'src/public/images/**/*';
 
 function imgOptimTask() {
   return src(IMG_PATH)
     .pipe(imagemin())
-    .pipe(gulp.dest('src/public/images'))
+    .pipe(dest('src/public/images'))
 }
 
 function watchTask() {
