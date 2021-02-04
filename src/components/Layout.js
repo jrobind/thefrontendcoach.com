a2080e1ab9c5f78e34d5a5ed56f9dbe7b8c648ab
James Robinson <jrobind@outlook.com>
2021-02-04 14:58:31 +0000

    prefetch and preload images and fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,9 +13,13 @@ export default function Layout({ children }) {
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://cdn.iubenda.com/" crossOrigin=""/>
+        <link rel="preload" href="/fonts/nunito-v14-latin-regular.woff2" as="font" />
+        <link rel="preload" href="/fonts/nunito-v16-latin-700.woff2" as="font" />
         <link rel="prefetch" href="/images/question.svg" as="image"/>
         <link rel="prefetch" href="/images/blog-home-blob-2.svg" as="image"/>
         <link rel="prefetch" href="/images/blog-home-blob.svg" as="image"/>
+        <link rel="prefetch" href="/images/logo-white-small.svg" as="image"/>
+        <link rel="prefetch" href="/images/logo-white-large.svg" as="image"/>
       </Head>
       <style jsx global>{`
         #__next {
