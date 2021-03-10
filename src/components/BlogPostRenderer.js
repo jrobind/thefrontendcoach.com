653e1723a30d3607e19fe710ae08f92a2d18b685
James Robinson <jrobind@outlook.com>
2021-03-10 17:30:27 +0000

    improve markup & formating changes

--- src/components/BlogPostRenderer.js
+++ src/components/BlogPostRenderer.js
@@ -1,23 +1,22 @@
-import dynamic from 'next/dynamic'
-import React, { useEffect } from 'react';
-import ReactMarkdown from 'react-markdown';
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import js from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript';
-import html from 'react-syntax-highlighter/dist/cjs/languages/hljs/htmlbars';
-import xml from 'react-syntax-highlighter/dist/cjs/languages/hljs/xml';
-import bash from 'react-syntax-highlighter/dist/cjs/languages/hljs/bash';
-import vs2015 from 'react-syntax-highlighter/dist/cjs/styles/hljs/vs2015';
+import dynamic from "next/dynamic";
+import React, { useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import js from "react-syntax-highlighter/dist/cjs/languages/hljs/javascript";
+import html from "react-syntax-highlighter/dist/cjs/languages/hljs/htmlbars";
+import xml from "react-syntax-highlighter/dist/cjs/languages/hljs/xml";
+import bash from "react-syntax-highlighter/dist/cjs/languages/hljs/bash";
+import vs2015 from "react-syntax-highlighter/dist/cjs/styles/hljs/vs2015";
 import Link from "next/link";
-import { rootURL } from '../lib/constants';
-import { useAppContext } from '../lib/context';
+import { rootURL } from "../lib/constants";
+import { useAppContext } from "../lib/context";
 
-SyntaxHighlighter.registerLanguage('javascript', js);
-SyntaxHighlighter.registerLanguage('html', html);
-SyntaxHighlighter.registerLanguage('bash', bash);
-SyntaxHighlighter.registerLanguage('xml', xml);
+SyntaxHighlighter.registerLanguage("javascript", js);
+SyntaxHighlighter.registerLanguage("html", html);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("xml", xml);
 
-const DynamicSharing = dynamic(() => import('./Sharing'));
-
+const DynamicSharing = dynamic(() => import("./Sharing"));
 
 const CodeBlock = ({ language, value }) => {
   return (
@@ -28,44 +27,45 @@ const CodeBlock = ({ language, value }) => {
 };
 
 const Image = ({ alt, src }) => {
-  return <img alt={alt} src={src} width="600" height="400"/>
-}
+  return <img alt={alt} src={src} width="600" height="400" />;
+};
 
 const flatten = (text, child) => {
-  return typeof child === 'string'
+  return typeof child === "string"
     ? text + child
     : React.Children.toArray(child.props.children).reduce(flatten, text);
-}
+};
 
 const HeadingRenderer = (props) => {
   const children = React.Children.toArray(props.children);
-  const text = children.reduce(flatten, '');
-  const slug = text.toLowerCase()
-    .replace(/\?/g, '')
-    .replace("'", '')
-    .replace(/\W/g, '-');
+  const text = children.reduce(flatten, "");
+  const slug = text
+    .toLowerCase()
+    .replace(/\?/g, "")
+    .replace("'", "")
+    .replace(/\W/g, "-");
 
-  return React.createElement('h' + props.level, { id: slug }, props.children);
-}
+  return React.createElement("h" + props.level, { id: slug }, props.children);
+};
 
 export default function MarkdownRenderer({ markdownBody, hasCodeBlock, slug }) {
   const { setLoading } = useAppContext();
 
   useEffect(() => setLoading(true), []);
 
-  const renderers =  hasCodeBlock ? 
-    { 
-      heading: HeadingRenderer,
-      code: CodeBlock,
-      image: Image, 
-    } : 
-    { 
-      heading: HeadingRenderer,
-      image: Image
-    };
+  const renderers = hasCodeBlock
+    ? {
+        heading: HeadingRenderer,
+        code: CodeBlock,
+        image: Image,
+      }
+    : {
+        heading: HeadingRenderer,
+        image: Image,
+      };
 
   return (
-    <>
+    <article>
       <section className="blog-post__content">
         <ReactMarkdown
           escapeHtml={true}
@@ -75,21 +75,22 @@ export default function MarkdownRenderer({ markdownBody, hasCodeBlock, slug }) {
       </section>
       <div className="flex justify-between my-6 md:my-7">
         <Link href="/blog">
-          <a 
-            style={{width: 'fit-content'}} 
+          <a
+            style={{ width: "fit-content" }}
             className="flex items-center text-base font-bold"
           >
-            <img 
-              alt="" 
-              src='/images/arrow-left.svg'
+            <img
+              alt=""
+              src="/images/arrow-left.svg"
               width="22"
-              height="22" 
-              style={{margin: '0 0 0 -8px'}}
-            /> Back to articles
+              height="22"
+              style={{ margin: "0 0 0 -8px" }}
+            />{" "}
+            Back to articles
           </a>
         </Link>
         <DynamicSharing rootURL={rootURL} slug={slug} />
       </div>
-    </>
+    </article>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import fs from "fs";
-import { NextSeo } from 'next-seo';
+import { NextSeo } from "next-seo";
 import BlogPost from "../components/BlogPost";
-import { getAllBlogPosts } from '../lib/api';
-import { rootURL } from '../lib/constants';
-import Wave from '../components/Wave';
-import ReviewCard from '../components/ReviewCard';
-
+import { getAllBlogPosts } from "../lib/api";
+import { rootURL } from "../lib/constants";
+import Wave from "../components/Wave";
+import ReviewCard from "../components/ReviewCard";
 
 const Home = ({ slugs, posts }) => (
   <div className="landing">
@@ -16,38 +15,76 @@ const Home = ({ slugs, posts }) => (
     />
     <section className="hero bg-main px-3 py-5 md:px-3">
       <div className="wrapper u-px-3 flex md:justify-between justify-center items-center">
-        <div className="text-white flex flex-col md:mr-4" style={{maxWidth: "650px"}}>
-          <h1 className="text-shadow">The Help You Need To Start Your Career as a Front-End Developer</h1>
+        <div
+          className="text-white flex flex-col md:mr-4"
+          style={{ maxWidth: "650px" }}
+        >
+          <h1 className="text-shadow">
+            The Help You Need To Start Your Career as a Front-End Developer
+          </h1>
           <p className="my-3 text-lg md:text-xl">
-            Thinking of becoming a front-end developer? The Front End Coach is here to help! A free support system to help you acquire the skills you need.
+            Thinking of becoming a front-end developer? The Front End Coach is
+            here to help! A free support system to help you acquire the skills
+            you need.
           </p>
           <Link href="/support">
-            <a 
+            <a
               className="hero-cta text-xl md:text-2xl text-tertiary"
-              style={{width: 'fit-content'}}>
-              <span 
+              style={{ width: "fit-content" }}
+            >
+              <span
                 className="border-b-2 inline-block font-bold"
-                style={{lineHeight: '1.2'}}>See support options</span>
-              <svg className="arrow ml-3 inline" width="27" height="24" fill="#FECB3E" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path stroke="#FECB3E" strokeWidth="2" d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/></svg>
+                style={{ lineHeight: "1.2" }}
+              >
+                See support options
+              </span>
+              <svg
+                className="arrow ml-3 inline"
+                width="27"
+                height="24"
+                fill="#FECB3E"
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              >
+                <path
+                  stroke="#FECB3E"
+                  strokeWidth="2"
+                  d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"
+                />
+              </svg>
             </a>
           </Link>
         </div>
-        <img className="img-shadow hidden h-auto md:block" alt="Man inspecting code block" src="./images/web-development.svg" width='450' height='338'/>
+        <img
+          className="img-shadow hidden h-auto md:block"
+          alt="Man inspecting code block"
+          src="./images/web-development.svg"
+          width="450"
+          height="338"
+        />
       </div>
     </section>
-    <Wave alignment="bottom"/>
+    <Wave alignment="bottom" />
 
     <section className="about px-3 my-7 md:my-8">
       <div className="wrapper flex flex-col md:flex-row md:justify-between items-center">
-        <img 
-          className="rounded max-w-250 md:max-w-340 w-full img-shadow mb-3 md:mb-0" 
+        <img
+          className="rounded max-w-250 md:max-w-340 w-full img-shadow mb-3 md:mb-0"
           src="/images/james-robinson.jpeg"
           alt="Photo of James Robinson on a mountain"
         />
         <div className="md:max-w-600 md:ml-4">
           <h2 className="mb-3 text-2xl md:text-3xl">About James Robinson</h2>
           <p className="mb-5 text-base md:text-lg">
-            Hey. I'm James Robinson, a self-taught front-end developer. I've been through the process myself, and I know just how frustrating and difficult it can be when first starting out. Since landing my first job in the industry I've worked on a variety of exciting projects, for high-profile industry-leading brands including Google. I'm here to share with you my honest, BS-free approach to acquiring the skills you <span className="font-bold">actually need</span> to become a front-end developer.
+            Hey. I'm James Robinson, a self-taught front-end developer. I've
+            been through the process myself, and I know just how frustrating and
+            difficult it can be when first starting out. Since landing my first
+            job in the industry I've worked on a variety of exciting projects,
+            for high-profile industry-leading brands including Google. I'm here
+            to share with you my honest, BS-free approach to acquiring the
+            skills you <span className="font-bold">actually need</span> to
+            become a front-end developer.
           </p>
           <Link href="/about">
             <a className="btn">MY JOURNEY</a>
@@ -56,17 +93,33 @@ const Home = ({ slugs, posts }) => (
       </div>
     </section>
 
-    <section className="services py-6 md:py-8" style={{backgroundColor: 'rgba(0, 0, 0, 0.025)'}}>
+    <section
+      className="services py-6 md:py-8"
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.025)" }}
+    >
       <div className="wrapper flex-col px-3">
         <div className="flex flex-col mb-5 md:flex-row justify-center">
           <div className="block justify-center flex-col mb-6 md:mb-3 items-center md:mr-7">
             <div className="flex justify-center items-center">
-              <img className="m-auto block img-shadow" alt="" src="./images/coach.svg" style={{maxWidth: '290px'}} width='450' height='350'/>
+              <img
+                className="m-auto block img-shadow"
+                alt=""
+                src="./images/coach.svg"
+                style={{ maxWidth: "290px" }}
+                width="450"
+                height="350"
+              />
             </div>
             <div className="text-center flex flex-1 flex-col items-center">
-              <h3 className="font-bold font-medium mb-3 text-lg md:text-xl">Support</h3>
-              <p className="mb-3 text-base md:text-lg" style={{maxWidth: '380px'}}>
-                Find the support you need by joining our Discord community. Alternatively, apply for a more personalised support.
+              <h3 className="font-bold font-medium mb-3 text-lg md:text-xl">
+                Support
+              </h3>
+              <p
+                className="mb-3 text-base md:text-lg"
+                style={{ maxWidth: "380px" }}
+              >
+                Find the support you need by joining our Discord community.
+                Alternatively, apply for a more personalised support.
               </p>
               <Link href="/support">
                 <a className="btn">GET SUPPORT</a>
@@ -75,69 +128,114 @@ const Home = ({ slugs, posts }) => (
           </div>
           <div className="flex flex-col mb-3 items-center">
             <div className="block justify-center items-center flex-1">
-              <img className="m-auto block img-shadow" alt="" src="./images/radio-presenter.svg" style={{width: '290px'}} width='450' height='350'/>
+              <img
+                className="m-auto block img-shadow"
+                alt=""
+                src="./images/radio-presenter.svg"
+                style={{ width: "290px" }}
+                width="450"
+                height="350"
+              />
             </div>
             <div className="flex 1 text-center flex flex-col items-center">
               <h3 className="font-bold mb-3 text-lg md:text-xl">Podcast</h3>
-              <p className="mb-3 text-base md:text-lg" style={{maxWidth: '380px'}}>
-                Interviews, advice on learning how to build your front-end skillset, getting your first job in the tech industry and more!
+              <p
+                className="mb-3 text-base md:text-lg"
+                style={{ maxWidth: "380px" }}
+              >
+                Interviews, advice on learning how to build your front-end
+                skillset, getting your first job in the tech industry and more!
               </p>
-                <a className="btn" href="https://anchor.fm/thefrontendcoach">LISTEN NOW</a>
+              <a className="btn" href="https://anchor.fm/thefrontendcoach">
+                LISTEN NOW
+              </a>
             </div>
           </div>
         </div>
-        <div className="rounded p-3 mb-4 shadow-md bg-white mt-6 md:mt-7 bg-blog-blob-landing bg-cover" style={{backgroundPosition: '-488px -271px'}}>
+        <article
+          className="rounded p-3 mb-4 shadow-md bg-white mt-6 md:mt-7 bg-blog-blob-landing bg-cover"
+          style={{ backgroundPosition: "-488px -271px" }}
+        >
           <div>
-            <h2 className="mb-4 m-auto text-2xl md:text-3xl">Latest blog posts</h2>
+            <h2 className="mb-4 m-auto text-2xl md:text-3xl">
+              Latest blog posts
+            </h2>
             <div className="flex justify-between grid gap-3 grid-cols-landing-blog-sm md:grid-cols-landing-blog-lg">
-              <BlogPost posts={posts} limit={4}/>
+              <BlogPost posts={posts} limit={4} />
             </div>
             <Link href="/blog">
               <a className="btn">READ THE BLOG</a>
             </Link>
           </div>
-        </div>
+        </article>
       </div>
     </section>
 
     <section className="wrapper px-3 mt-5 md:mt-6 my-7">
       <div className="flex flex-col md:flex-row md:justify-between items-center">
-      <img className="block img-shadow" style={{maxWidth: '450px'}} alt="Scrum board illustration" src="./images/scrum-board.svg"/>
+        <img
+          className="block img-shadow"
+          style={{ maxWidth: "450px" }}
+          alt="Scrum board illustration"
+          src="./images/scrum-board.svg"
+        />
         <div className="md:max-w-600">
-          <h2 className="mb-3 text-2xl md:text-3xl">Contribute to Open Source</h2>
+          <h2 className="mb-3 text-2xl md:text-3xl">
+            Contribute to Open Source
+          </h2>
           <p className="mb-5 text-base md:text-lg">
-            The Front End Coach open source project has been set up to create a welcoming and friendly learning environment, so you can feel safe making mistakes. This is also an opportunity to receive feedback and guidance whilst collaborating with other developers.
+            The Front End Coach open source project has been set up to create a
+            welcoming and friendly learning environment, so you can feel safe
+            making mistakes. This is also an opportunity to receive feedback and
+            guidance whilst collaborating with other developers.
           </p>
-          <a href="https://github.com/jrobind/dev-jot" className="btn mt-5">GO TO PROJECT</a>
+          <a href="https://github.com/jrobind/dev-jot" className="btn mt-5">
+            GO TO PROJECT
+          </a>
         </div>
       </div>
     </section>
 
     <section>
       <div className="wrapper px-3 mb-6 md:mb-8">
-          <div className="px-3 pb-4 bg-main text-white rounded shadow-md">
-            <img className="m-auto block" style={{maxWidth: '400px'}} alt="Discord logo" src="./images/discord-logo.svg"/>
-            <div className="flex items-center flex-col mt-3 m-auto max-w-600">
-              <p className="text-base md:text-lg mb-3 text-center">Don't learn in isolation! Join other like-minded, aspiring developers and myself. This is a highly inclusive and supportive community for those looking for help at whatever stage of their journey to become a front-end developer.</p>
-              <a
-                className="hero-cta text-xl md:text-2xl font-bold"
-                style={{width: 'fit-content'}}
-                href="https://discord.gg/YMsd9sgZ4f"
-              >
-                <span className="border-b-2">Join the community</span>
-              </a>
-            </div>
+        <div className="px-3 pb-4 bg-main text-white rounded shadow-md">
+          <img
+            className="m-auto block"
+            style={{ maxWidth: "400px" }}
+            alt="Discord logo"
+            src="./images/discord-logo.svg"
+          />
+          <div className="flex items-center flex-col mt-3 m-auto max-w-600">
+            <p className="text-base md:text-lg mb-3 text-center">
+              Don't learn in isolation! Join other like-minded, aspiring
+              developers and myself. This is a highly inclusive and supportive
+              community for those looking for help at whatever stage of their
+              journey to become a front-end developer.
+            </p>
+            <a
+              className="hero-cta text-xl md:text-2xl font-bold"
+              style={{ width: "fit-content" }}
+              href="https://discord.gg/YMsd9sgZ4f"
+            >
+              <span className="border-b-2">Join the community</span>
+            </a>
           </div>
         </div>
+      </div>
     </section>
 
     <section className="wrapper mt-3">
       <div className="px-3">
-        <h2 className="text-2xl md:text-3xl text-center">Mentee Testimonials</h2>
+        <h2 className="text-2xl md:text-3xl text-center">
+          Mentee Testimonials
+        </h2>
         <p className="m-auto text-center mb-5 mt-3 text-base md:text-lg max-w-600">
-          1:1 sessions provide you with the right level of accountability and support to help you reach your development goals.
+          1:1 sessions provide you with the right level of accountability and
+          support to help you reach your development goals.
         </p>
-        <a href="/support#direct-mentorship" className="btn mb-7 m-auto">APPLY NOW</a>
+        <a href="/support#direct-mentorship" className="btn mb-7 m-auto">
+          APPLY NOW
+        </a>
       </div>
       <ReviewCard />
     </section>
@@ -149,9 +247,9 @@ export const getStaticProps = async () => {
 
   return {
     props: {
-      slugs: files.map(filename => filename.replace(".md", "")),
+      slugs: files.map((filename) => filename.replace(".md", "")),
       posts: getAllBlogPosts(),
-    }
+    },
   };
 };
 
